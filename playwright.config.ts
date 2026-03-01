4bf70ca019527b49d9939cd82d02e5e982659d9a
yusufkaracaburun <ykaracaburun@gmail.com>
2026-03-01 13:15:52 +0100

    final steps

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
     retries: process.env.CI ? 2 : 0,
     workers: 1,
     reporter: 'list',
-    timeout: 15000,
+    timeout: 30000,
     expect: {
-        timeout: 5000,
+        timeout: 15000,
     },
     use: {
         baseURL: 'http://central.test',
